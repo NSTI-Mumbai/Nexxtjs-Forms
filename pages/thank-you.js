f7c888c2cb198c7bf6c38fbbd6cd7157fe05d5f9
n0step_ <krushnavyas06@gmail.com>
2024-10-23 14:05:16 +0000

    feat: thank you page

---
+++ pages/thank-you.js
@@ -0,0 +1,9 @@
+export default function ThankYouPage() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-white p-4">
+        <h1 className="text-4xl font-extrabold text-indigo-700 mb-4">Thank you!</h1>
+        <p className="text-lg text-gray-600">Thank you for filling the form. We have received your responses.</p>
+      </div>
+    );
+  }
+  
